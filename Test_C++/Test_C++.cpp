a1b542e07d1ce380d2302d65d54382427ca83d23
Benoit BARRAU <bbarrau@artfx.fr>
2022-03-14 11:17:59 +0100

    Test Push

---
+++ Test_C++/Test_C++.cpp
@@ -0,0 +1,15 @@
+// Test_C++.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "This is programming cours !\n";
+    cout << "Class is gonna crasher sang !\n";
+    cout << "FDP !\n\n";
+    cout << "JE MAPPELLE MICKEY OHOHOHOHOHOHO\n";
+
+}
